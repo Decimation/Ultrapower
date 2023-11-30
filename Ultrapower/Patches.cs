0820b185abddc0f7ce233a00170cd3f3610e6afb
Read <decimation001@gmail.com>
2023-11-30 01:47:38 -0600

    working

--- Ultrapower/Patches.cs
+++ Ultrapower/Patches.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using BepInEx;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.Networking;
+using Logger = BepInEx.Logging.Logger;
 
 namespace Ultrapower
 {
@@ -36,6 +40,32 @@ namespace Ultrapower
 	public static class HitTweak
 	{
 
+		static AudioClip LoadClip(string path)
+		{
+			Debug.Log(path);
+			AudioClip clip = null;
+
+			using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(path, AudioType.WAV)) {
+				uwr.SendWebRequest();
+
+				// wrap tasks in try/catch, otherwise it'll fail silently
+				try {
+					while (!uwr.isDone) 
+						_ = 0;
+
+					if (uwr.isNetworkError || uwr.isHttpError) Debug.Log($"{uwr.error}");
+					else {
+						clip = DownloadHandlerAudioClip.GetContent(uwr);
+					}
+				}
+				catch (Exception err) {
+					Debug.Log($"{err.Message}, {err.StackTrace}");
+				}
+			}
+
+			return clip;
+		}
+
 		//public void DeliverDamage(GameObject target, Vector3 force, Vector3 hitPoint, float multiplier, bool tryForExplode, float critMultiplier = 0F, GameObject sourceWeapon = null, bool ignoreTotalDamageTakenMultiplier = false)
 
 		[HarmonyPatch(typeof(EnemyIdentifier), nameof(EnemyIdentifier.DeliverDamage))]
@@ -44,8 +74,23 @@ namespace Ultrapower
 		                          float multiplier, bool tryForExplode, float critMultiplier, GameObject sourceWeapon,
 		                          bool ignoreTotalDamageTakenMultiplier)
 		{
+
 			Debug.Log($"butt {__instance}");
-			
+
+			try {
+				var cl =  LoadClip("file://" + @"C:\Users\Deci\VSProjects\Ultrapower\Ultrapower\bottlecap.wav");
+
+				Debug.Log($"{cl}");
+				var src=target.gameObject.AddComponent<AudioSource>();
+				src.clip = cl;
+				src.name = "bottlecap";
+				src.Play();
+				// var asss = new AudioSource() { clip = cl, name = "bottlecap"};
+				// asss.Play();
+			}
+			catch (Exception e) {
+				Debug.LogError(e);
+			}
 		}
 
 	}
---
+++ Ultrapower/copy_to_dir.bat
@@ -0,0 +1,2 @@
+@echo off
+copy "bin\Release\netstandard2.0\Ultrapower.dll" "J:\Program Files (x86)\Steam\steamapps\common\ULTRAKILL\BepInEx\plugins"
